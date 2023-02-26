2c932b36ca5ad9de066f6009ca18563801f361b4
Jean-BaptisteEmanuelZorg <114926614+Jean-BaptisteEmanuelZorg@users.noreply.github.com>
2023-02-27 00:32:41 +0200

    rewrite sortByField method

--- src/main/java/FlowerStore/ItemManager.java
+++ src/main/java/FlowerStore/ItemManager.java
@@ -5,6 +5,18 @@ import java.util.*;
 public class ItemManager {
     private List<Item> items;
 
+    private static final Map<String, Comparator<Item>> LIST_COMPARATOR;
+
+    static {
+        final Map<String, Comparator<Item>>
+                comparators = new HashMap<>();
+        comparators.put("itemName", (i1, i2) -> (i1.getItemName().compareTo(i2.getItemName())));
+        comparators.put("price", (i1, i2) -> i1.getPrice() - i2.getPrice());
+        comparators.put("description", (i1, i2) -> (i1.getDescription().compareTo(i2.getDescription())));
+
+        LIST_COMPARATOR = Collections.unmodifiableMap(comparators);
+    }
+
     public ItemManager() {
 
         this.items = new ArrayList<>();
@@ -23,43 +35,13 @@ public class ItemManager {
         return "ItemManager\n\n" +
                 "items:\n" + items.toString();
     }
-
     public void sortByField(String field, SortOrder order){
-        switch (field){
-            case "itemName":
-                if (order.equals(SortOrder.ASC)) {
-                    Collections.sort(this.items, (i1, i2) -> (i1.getItemName().compareTo(i2.getItemName())));
-                    System.out.println("Sorted by itemName (ASC)");
-                } else if (order.equals(SortOrder.DESC)) {
-                    Collections.sort(this.items, (i1, i2) -> (i1.getItemName().compareTo(i2.getItemName())));
-                    Collections.reverse(this.items);
-                    System.out.println("Sorted by itemName (DESC)");
-                }
-                break;
-            case "price":
-                if (order.equals(SortOrder.ASC)) {
-                    Collections.sort(this.items, (i1, i2) -> i1.getPrice() - i2.getPrice());
-                    System.out.println("Sorted by price (ASC)");
-                } else if (order.equals(SortOrder.DESC)) {
-                    Collections.sort(this.items, (i1, i2) -> i1.getPrice() - i2.getPrice());
-                    Collections.reverse(this.items);
-                    System.out.println("Sorted by price (DESC)");
-                }
-                break;
-            case "description":
-                if (order.equals(SortOrder.ASC)) {
-                    Collections.sort(this.items, (i1, i2) -> (i1.getDescription().compareTo(i2.getDescription())));
-                    System.out.println("Sorted by description (ASC)");
-                } else if (order.equals(SortOrder.DESC)) {
-                    Collections.sort(this.items, (i1, i2) -> (i1.getDescription().compareTo(i2.getDescription())));
-                    Collections.reverse(this.items);
-                    System.out.println("Sorted by description (DESC)");
-                }
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + field);
+        Comparator<Item> comparator = LIST_COMPARATOR.get(field);
+        if (order.equals(SortOrder.ASC)){
+            Collections.sort(this.items, comparator);
+        } else if (order.equals(SortOrder.DESC)){
+            Collections.sort(this.items, comparator);
+            Collections.reverse(this.items);
         }
-
-    }
-
+        }
 }
