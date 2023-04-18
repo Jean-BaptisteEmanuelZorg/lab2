b3204e59cb3e97e481f728184bbf4667b3c3d234
Jean-BaptisteEmanuelZorg <114926614+Jean-BaptisteEmanuelZorg@users.noreply.github.com>
2023-04-18 19:30:51 +0300

    Refactored itemName to name. Comparators are put into LIST_COMPARATOR directly. Sort order changed to boolean. sortByField method now have return statement. ItemManager class covered with tests. Logger added. First block of logic from compileBouquet method rewritten using streams. And other minor changes according to comments

--- src/main/java/FlowerStore/Main.java
+++ src/main/java/FlowerStore/Main.java
@@ -1,11 +1,16 @@
 package FlowerStore;
 
 
-import java.util.ArrayList;
+import org.slf4j.*;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         ItemManager manager = new ItemManager();
         manager.addItem(new BouquetFlower(BouquetFlowerType.ROSE, 55, "special", "blue"));
@@ -26,17 +31,19 @@ public class Main {
         manager.addItem(new BouquetFlower(BouquetFlowerType.DAISY, 44, "average", "red"));
         manager.addItem(new BouquetFlower(BouquetFlowerType.IRIS, 62, "great", "purple"));
         manager.addItem(new BouquetFlower(BouquetFlowerType.PEONY, 70, "perfect", "pink"));
+        manager.addItem(new HousePlant("plant", 55, "description", 5, true, HousePlantType.CITRUS));
+        manager.addItem(new Pot("vase", 45, "some vase", 20, 40, 99, "glass"));
+        manager.addItem(new Soil("dirt", 78, "some soil", 4.5, "1,2,3", 5, 5, SoilType.UNIVERSAL));
 
         BouquetConfiguration bouquetConfiguration = new BouquetConfiguration("red",
-                new PriceRange(0, 500),
+                new PriceRange(333, 500),
                 Arrays.asList("ROSE", "TULIP", "DAISY"),
                 9,
                 "blue paper"
         );
-
         List<BouquetFlower> bouquetProposal =  manager.compileBouquet(bouquetConfiguration,
                 manager.getItems());
+        log.info("{}", bouquetProposal);
 
-        System.out.println(bouquetProposal);
     }
 }
